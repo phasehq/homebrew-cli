c5becd2aa5acb16201780fb705b39e283e99f6cc
Nimish <85357445+nimish-ks@users.noreply.github.com>
2023-11-18 15:52:27 +0530

    Update phase.rb

--- phase.rb
+++ phase.rb
@@ -1,19 +1,32 @@
 class Phase < Formula
   desc "Securely manage your secrets and environment variables with Phase."
   homepage "https://github.com/phasehq/cli"
-  url "https://github.com/phasehq/cli/releases/download/v1.7.4/phase_cli_macos_amd64_1.7.4"
-  sha256 "09c345463d91536ee98d9cf0b81241d24c483de038ca4889ad6abc74011e5e50"
-  version "1.7.4"
+  url "https://github.com/phasehq/cli/releases/download/v1.8.0/phase_cli_macos_amd64_1.8.0.zip"
+  sha256 "3fb590aa14852ed5f9c7743bdf0dbc58e0b0c4c94015edb24633a4e241fce93e"
+  version "1.8.0"
 
   def install
-    # Rename the binary to "phase" (if needed)
-    mv "phase_cli_macos_amd64_1.7.4", "phase" unless File.exist?("phase")
-    
-    # Set permissions before installation
-    chmod 0755, "phase"
-    
-    # Install the binary
-    bin.install "phase"
+    # Extract the contents of the zip file
+    system "unzip", "phase_cli_macos_amd64_#{version}.zip", "-d", "phase"
+
+    # Move into the extracted directory
+    Dir.chdir("phase/macOS-binary/phase") do
+      # Rename the binary to "phase" (if needed)
+      mv "phase_cli_macos_amd64_#{version}", "phase" unless File.exist?("phase")
+      
+      # Set permissions for the binary
+      chmod 0755, "phase"
+
+      # Install the binary
+      bin.install "phase"
+
+      # Install the _internal directory
+      prefix.install "_internal"
+    end
   end
 
+  test do
+    # Write a test for your application here, if possible
+    system "#{bin}/phase", "--version"
+  end
 end
