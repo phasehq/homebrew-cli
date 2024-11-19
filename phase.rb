847814d8fb3ec241a52eb5b2b9520378f818a946
Nimish <85357445+nimish-ks@users.noreply.github.com>
2024-11-19 20:37:14 +0530

    Update phase.rb

--- phase.rb
+++ phase.rb
@@ -1,9 +1,9 @@
 class Phase < Formula
   desc "Securely manage your secrets and environment variables with Phase."
   homepage "https://github.com/phasehq/cli"
-  url "https://github.com/phasehq/cli/releases/download/v1.18.5/phase_cli_macos_amd64_1.18.5.zip"
-  sha256 "97810332a003de65fce41a8f90ce3aa5068c95f84ed08b683ee9ea736621afc8"
-  version "1.18.5"
+  url "https://github.com/phasehq/cli/releases/download/v1.18.6/phase_cli_macos_amd64_1.18.6.zip"
+  sha256 "febe79facea83d481dae62cafb091c6ddd4026ab1d1a78d1b6022b24e6bca593"
+  version "1.18.6"
 
   def install
     # Install the phase binary
