e09646a8bf658fee02cb16182cad6e93a3af0bf6
Nimish <nimish@phase.dev>
2025-10-01 16:26:01 +0530

    fix: hash

--- phase.rb
+++ phase.rb
@@ -2,7 +2,7 @@ class Phase < Formula
   desc "Securely manage your secrets and environment variables with Phase."
   homepage "https://github.com/phasehq/cli"
   url "https://github.com/phasehq/cli/releases/download/v1.20.0/phase_cli_macos_amd64_1.20.0.zip"
-  sha256 "d69f0b9d0df5b87a7514e8a62c6a49f935fdba90372b66fee82121f63096104f"
+  sha256 "86c8644df6453a26f9380c5bc33566445973ec58f179547fa4575aca0f3f0d6b"
   version "1.20.0"
 
   def install
